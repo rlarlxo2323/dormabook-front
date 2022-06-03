af4b532dcf7db34cf8345401651125112d017b21
김기태 <kimgitae@GiRi.local>
2022-06-04 07:02:20 +0900

    modify login js html

--- login/js/login.js
+++ login/js/login.js
@@ -21,3 +21,35 @@ var signUp = document.getElementById("signUp");
 signUp.addEventListener('click', function (){
     window.location.href = "https://dormabook.shop/signup/guide/"
 });
+
+//login jwt
+$(document).ready(function($) {
+    $("#login").click(function () {
+        var xhr = new XMLHttpRequest();
+        var getMemberId = $("#id").val();
+        var getMemberPwd = $("#password").val();
+        $.ajax({
+            url: "https://dormabook.shop/api/auth/login",
+            type: "post",
+            contentType : 'application/json; charset=UTF-8',
+            data: JSON.stringify({
+                memberId: getMemberId,
+                memberPwd : getMemberPwd
+            }),
+            datatype: "json",
+            success: function (data) {
+                console.log(data);
+                const dtos = JSON.stringify(data);
+                console.log(dtos);
+                var tmp = JSON.parse(dtos);
+                var token = 'Bearer '+ tmp.accessToken
+                console.log(tmp.accessToken);
+                sessionStorage.setItem('jwt',token);
+                window.location.href = "https://dormabook.shop/community"
+            },
+            error: function () {
+                //
+            }
+        })
+    })
+});
